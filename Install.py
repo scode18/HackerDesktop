d0147de4bbb136aaeddd4bd99acde2dcfa13f7a8
Solomon <98618381+scode18@users.noreply.github.com>
2024-06-25 14:26:48 +0300

    ⚜️улучшил док

--- Install.py
+++ Install.py
@@ -8,16 +8,74 @@ from win32com.client import Dispatch
 # Путь к утилите busybox.exe
 busybox_path = r"Utils\\busybox.exe"
 
-# URL для скачивания файла Installers.7z
-file_url_installers = "https://github.com/scode18/Installers-for-HackerDesktop/releases/download/v1/Installers.7z"
+# Список URL для скачивания файлов
+file_urls = [
+    "https://github.com/scode18/Installers-for-HackerDesktop/releases/download/v1/Installers.7z",
+    "https://github.com/scode18/HackerDesktop/raw/main/Archives.7z",
+    "https://github.com/scode18/HackerDesktop/raw/main/Files.7z"
+]
 
-# Команда для скачивания файла Installers.7z с помощью busybox.exe wget
-download_command_installers = [busybox_path, "wget", file_url_installers]
+# Команды для скачивания файлов с помощью busybox.exe wget
+download_commands = [[busybox_path, "wget", url] for url in file_urls]
 
-# Выполнение команды скачивания Installers.7z
-subprocess.run(download_command_installers, check=True)
+# Выполнение команд для скачивания файлов
+for i, command in enumerate(download_commands):
+    subprocess.run(command, check=True)
+    print(f"Файл {file_urls[i].split('/')[-1]} успешно скачан.")
 
-print("Файл Installers.7z успешно скачан с помощью busybox.exe wget и будет распакован в папку Installers.")
+# Путь к утилите 7za.exe
+seven_zip_path = r"Utils\\7za.exe"
+
+# Путь исходных архивов и файлов
+source_files = [
+    "Installers.7z"
+]
+
+# Распаковка всех скачанных файлов
+for file in source_files:
+    extract_path = "."
+    if file.endswith(".7z"):
+        extract_path = file.split(".")[0]  # Для архивов создаем папку с именем без расширения
+    extract_command = [seven_zip_path, "x", file, f"-o{extract_path}"]
+    subprocess.run(extract_command, check=True)
+    print(f"Файл {file} успешно распакован в папку {extract_path}.")
+
+print("Все файлы успешно скачаны и распакованы.")
+
+# Список имен файлов для перемещения
+files_to_move = [
+    "Wallpaper.png",
+    "finder.png",
+    "Trash Dark.png",
+    "Trash Dark Full.png"
+]
+
+# Путь назначения
+destination_path = r"C:\\"
+
+# Проверка существования файлов и их перемещение
+for file_name in files_to_move:
+    if os.path.exists(file_name):
+        shutil.move(file_name, os.path.join(destination_path, file_name))
+        print(f"Файл {file_name} успешно перемещен в {destination_path}")
+    else:
+        print(f"Файл {file_name} не найден и не был перемещен.")
+
+print("Все операции завершены.")
+
+# # Путь к утилите busybox.exe
+# busybox_path = r"Utils\\busybox.exe"
+
+# # URL для скачивания файла Installers.7z
+# file_url_installers = "https://github.com/scode18/Installers-for-HackerDesktop/releases/download/v1/Installers.7z"
+
+# # Команда для скачивания файла Installers.7z с помощью busybox.exe wget
+# download_command_installers = [busybox_path, "wget", file_url_installers]
+
+# # Выполнение команды скачивания Installers.7z
+# subprocess.run(download_command_installers, check=True)
+
+# print("Файл Installers.7z успешно скачан с помощью busybox.exe wget и будет распакован в папку Installers.")
 
 # Путь к утилите 7za.exe
 seven_zip_path = r"Utils\\7za.exe"
@@ -34,40 +92,54 @@ extract_command_installers = [seven_zip_path, "x", archive_file_installers, f"-o
 # Выполнение команды распаковки Installers.7z
 subprocess.run(extract_command_installers, check=True)
 
-print("Файл Installers.7z успешно распакован в папку Installers.")
+print("Файл Installers.7z успешно распакован.")
 
-# URL для скачивания файла Archives.7z
-file_url_archives = "https://github.com/scode18/HackerDesktop/raw/main/Archives.7z"
+# Путь к скачанному архиву Files.7z
+archive_file_installers = "Files.7z"
 
-# Команда для скачивания файла Archives.7z с помощью busybox.exe wget
-download_command_archives = [busybox_path, "wget", file_url_archives]
+# Путь для распаковки архива Files.7z
+extract_path_installers = "."
 
-# Выполнение команды скачивания Archives.7z
-subprocess.run(download_command_archives, check=True)
+# Команда для распаковки архива Files.7z с помощью 7za.exe
+extract_command_installers = [seven_zip_path, "x", archive_file_installers, f"-o{extract_path_installers}"]
 
-print("Файл Archives.7z успешно скачан с помощью busybox.exe wget и будет распакован в текущую директорию.")
+# Выполнение команды распаковки Files.7z
+subprocess.run(extract_command_installers, check=True)
 
-# Путь к скачанному архиву Archives.7z
-archive_file_archives = "Archives.7z"
+print("Файл Files.7z успешно распакован.")
 
-# Путь для распаковки архива Archives.7z (в текущую директорию)
-extract_path_archives = "."
+# # URL для скачивания файла Archives.7z
+# file_url_archives = "https://github.com/scode18/HackerDesktop/raw/main/Archives.7z"
 
-# Команда для распаковки архива Archives.7z с помощью 7za.exe
-extract_command_archives = [seven_zip_path, "x", archive_file_archives, f"-o{extract_path_archives}"]
+# # Команда для скачивания файла Archives.7z с помощью busybox.exe wget
+# download_command_archives = [busybox_path, "wget", file_url_archives]
 
-# Выполнение команды распаковки Archives.7z
-subprocess.run(extract_command_archives, check=True)
+# # Выполнение команды скачивания Archives.7z
+# subprocess.run(download_command_archives, check=True)
 
-print("Файл Archives.7z успешно распакован в текущую директорию.")
+# print("Файл Archives.7z успешно скачан с помощью busybox.exe wget и будет распакован в текущую директорию.")
+
+# # Путь к скачанному архиву Archives.7z
+# archive_file_archives = "Archives.7z"
+
+# # Путь для распаковки архива Archives.7z (в текущую директорию)
+# extract_path_archives = "."
+
+# # Команда для распаковки архива Archives.7z с помощью 7za.exe
+# extract_command_archives = [seven_zip_path, "x", archive_file_archives, f"-o{extract_path_archives}"]
+
+# # Выполнение команды распаковки Archives.7z
+# subprocess.run(extract_command_archives, check=True)
+
+# print("Файл Archives.7z успешно распакован в текущую директорию.")
 
 # Пути для установки программ
 nexus_winstep_path = os.path.join(os.getcwd(), "Installers\\NexusSetup.exe")
 rainmeter_path = os.path.join(os.getcwd(), "Installers\\Rainmeter-4.5.18.exe")
 
 # Пути к файлам и папкам
-wallpaper_file = "Wallpaper.png"
-wallpaper_dest = os.path.join("C:\\", "Wallpaper.png")
+# wallpaper_file = "Wallpaper.png"
+# wallpaper_dest = os.path.join("C:\\", "Wallpaper.png")
 
 winstep_archive = "Archives\\Winstep.7z"
 winstep_dest = os.path.join("C:\\", "Users", "Public", "Documents", "Winstep")
@@ -144,7 +216,7 @@ if __name__ == "__main__":
         exit(1)
 
     # Перемещаем Wallpaper.png в корень диска C
-    move_file(wallpaper_file, wallpaper_dest)
+    # move_file(wallpaper_file, wallpaper_dest)
 
     # Распаковываем Winstep.7z в C:\Users\Public\Documents\Winstep
     extract_archive(winstep_archive, winstep_dest)
